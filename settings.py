406bc0092abe1e09649627a793f4badd820be2a9
Tushar Gautam <tushar.rishav@gmail.com>
2015-07-23 11:01:01 +0530

    setting :white_check_mark:

--- settings.py
+++ settings.py
@@ -1,11 +1,13 @@
 from random import randint
+
+
 class config():
-	CLIENT_ID='75e82w7gbp82nr'
-	CLIENT_SECRET='QQhpeWSTUiflDFnY'
-	AUTH_URL="https://www.linkedin.com/uas/oauth2/authorization"
-	ACCESS_URL="https://www.linkedin.com/uas/oauth2/accessToken"
-	REDIRECT_URL="http://localhost:8080/auth/linkedin/callback"
-	STATE=str(randint(10000,99999))
-	SCOPE="r_basicprofile+r_emailaddress"
-	HOST=''
-	PORT=8080
+    CLIENT_ID = '75e82w7gbp82nr'
+    CLIENT_SECRET = 'QQhpeWSTUiflDFnY'
+    AUTH_URL = "https://www.linkedin.com/uas/oauth2/authorization"
+    ACCESS_URL = "https://www.linkedin.com/uas/oauth2/accessToken"
+    REDIRECT_URL = "http://localhost:8080/auth/linkedin/callback"
+    STATE = str(randint(10000, 99999))
+    SCOPE = "r_basicprofile+r_emailaddress"
+    HOST = ''
+    PORT = 8080
